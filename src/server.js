2ebdfb54145544ab6920be846ac75b0cf7df211c
syniepolskyi84 <e.syniepolskyi@gmail.com>
2025-04-06 23:43:09 +0300

    corection 2

--- src/server.js
+++ src/server.js
@@ -23,7 +23,8 @@ export const startServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello World',
+      message: 'Server start successfully!',
+
     });
   });
 
