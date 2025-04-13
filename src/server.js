ac243d79fbcc3e5a3de0a0518afb3875b7377fd8
syniepolskyi84 <e.syniepolskyi@gmail.com>
2025-04-13 22:02:25 +0300

    Post, patch, delete

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
+
+import { logger } from './middlewares/logger.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts } from './services/contacts.js';
-import { getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -13,61 +15,16 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  // app.use(logger);
 
   app.get('/', (req, res) => {
-    res.json({
-      message: 'Server start successfully!',
-
-    });
+    res.json({ message: 'Server start successfully!' });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use('/contacts', contactsRouter);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(notFoundHandler);   // 404 – не знайдено
+  app.use(errorHandler);      // 500 – внутрішні помилки
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
