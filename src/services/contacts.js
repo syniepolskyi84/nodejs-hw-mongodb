fa03dfc1356dcd31a364a80adab4ac7083f378ad
syniepolskyi84 <e.syniepolskyi@gmail.com>
2025-04-21 18:34:44 +0300

    corection

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -40,11 +40,6 @@ export const getAllContacts = async ({
   };
 };
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-};
-
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
