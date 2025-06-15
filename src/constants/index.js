b96a9262c5c930a1bc6c3c04d298f7accd2d2071
syniepolskyi84 <e.syniepolskyi@gmail.com>
2025-06-15 16:09:39 +0300

    corection constants index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,9 +33,9 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  CLOUD_API_KEY: 'API_KEY',
-  CLOUD_API_SECRET: 'API_SECRET',
+  CLOUD_NAME: 'CLOUDINARY_CLOUD_NAME',
+  API_KEY: 'CLOUDINARY_API_KEY',
+  API_SECRET: 'CLOUDINARY_API_SECRET',
 };
 
 export const FEATURE = {
